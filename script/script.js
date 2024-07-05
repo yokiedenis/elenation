604e3eeb6bcff141d9eb7f169528a3dccc913ff0
edwinndagijimana <edwinndagijimana@gmail.com>
2024-07-06 01:41:55 +0300

    second

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 let themeSwitchButton = document.getElementById("themeSwitch")
-themeSwitchButton.addEventListener("click",function(){
-    document.body.classList.toggle("color");
-    document.querySelector("a").classList.toggle("color")
-})
+// themeSwitchButton.addEventListener("click",function(){
+    
+//     document.querySelector("a").classList.toggle("color")
+// })
 // search feature to search within page
 // function search(){
 //     var searchText=$("#searchText").val();
@@ -40,5 +40,37 @@ function sideBar() {
   body.appendChild(sideBarElement);
 }
 
+let wr = document.querySelector('.wrapper');
+let bg = document.querySelector('.background');
+let bg2 = document.querySelector('.background-2');
+let sun = document.querySelector('.sun');
+let circles = document.querySelectorAll('.sun-circle');
+let spots = document.querySelectorAll('.sun-spot');
 
+let flag = false;
+wr.addEventListener('click', () => {
+  if(flag == false)
+  {
+    bg.classList.add('background_active');
+    wr.classList.add('wrapper_active');
+    sun.classList.add('sun_active');
+    document.body.classList.toggle("color");
+    circles[0].classList.add('sun-circle_active');
+    circles[1].classList.add('sun-circle-two_active');
+    circles[2].classList.add('sun-circle-three_active');
+    bg2.classList.add('background-2_active');
+   flag = true;
+  }
+  else{
+    bg.classList.remove('background_active');
+    wr.classList.remove('wrapper_active');
+    sun.classList.remove('sun_active');
+    document.body.classList.toggle("color");
+    circles[0].classList.remove('sun-circle_active');
+    circles[1].classList.remove('sun-circle-two_active');
+    circles[2].classList.remove('sun-circle-three_active');
+    bg2.classList.remove('background-2_active');
+    flag = false;
+  }
+})
 
