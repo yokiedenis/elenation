5aa44224a143c962db587a0410af40f7907c386f
unknown <yokasdenis@gmail.com>
2024-09-12 11:26:35 +0300

    geterrioal

--- script/blogscript.js
+++ script/blogscript.js
@@ -184,43 +184,45 @@ function displayDataAll() {
     card.className = "card";
     card.id = "card";
     card.innerHTML = `   <div
-      class="cardImage"
-      style="
-        background-image: url(${data[i].imageSrc});
-        background-repeat: no-repeat;
-        background-size: cover;
-      "
-    ></div>
-    <span class="cardType">${data[i].type}</span>
-    <p class="cardName">${data[i].name}</p>
-    <div class="cardFooter">
-        <p>${data[i].time}</p>
-        <div style="display:flex;align-items:center;">
-          <img src="like.png" alt="like" loading="lazy" />
-          <svg
-              style="margin:0px 5px;"
-            xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            height="22"
-            viewBox="0 0 22 22"
-            fill="none"
-          >
-            <g clip-path="url(#clip0_3238_140)">
-              <path
-                d="M18.9347 15.5907C20.0485 13.6639 20.4228 11.3976 19.9877 9.21499C19.5526 7.03241 18.3377 5.08289 16.5701 3.73063C14.8025 2.37837 12.6031 1.71587 10.3827 1.86689C8.16225 2.01791 6.07277 2.97212 4.50454 4.55127C2.93632 6.13041 1.99663 8.22647 1.86103 10.4479C1.72542 12.6693 2.40317 14.8641 3.76766 16.6222C5.13215 18.3804 7.09006 19.5817 9.27561 20.0017C11.4612 20.4217 13.7249 20.0316 15.6438 18.9044L20.1667 20.1667L18.9347 15.5907Z"
-                stroke="#252525"
-                stroke-width="2"
-                stroke-linejoin="round"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_3238_140">
-                <rect width="22" height="22" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
-      </div>`;
+        class="cardImage"
+        style="
+          background-image: url(${data[i].imageSrc});
+          background-repeat: no-repeat;
+          background-size: cover;
+        "
+      ></div>
+      <span class="cardType">${data[i].type}</span>
+      <p class="cardName">${data[i].name}</p>
+      <div class="cardFooter">
+          <p>${data[i].time}</p>
+          <div style="display:flex;align-items:center;">
+            <img src="like.png" alt="like" loading="lazy" />
+            <p>${data[i].Text}</p>
+            <svg
+                style="margin:0px 5px;"
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_3238_140)">
+                <path
+                  d="M18.9347 15.5907C20.0485 13.6639 20.4228 11.3976 19.9877 9.21499C19.5526 7.03241 18.3377 5.08289 16.5701 3.73063C14.8025 2.37837 12.6031 1.71587 10.3827 1.86689C8.16225 2.01791 6.07277 2.97212 4.50454 4.55127C2.93632 6.13041 1.99663 8.22647 1.86103 10.4479C1.72542 12.6693 2.40317 14.8641 3.76766 16.6222C5.13215 18.3804 7.09006 19.5817 9.27561 20.0017C11.4612 20.4217 13.7249 20.0316 15.6438 18.9044L20.1667 20.1667L18.9347 15.5907Z"
+                  stroke="#252525"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_3238_140">
+                  <rect width="22" height="22" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+              
+          </div>
+        </div>`;
     mainBody.appendChild(card);
   }
 }
